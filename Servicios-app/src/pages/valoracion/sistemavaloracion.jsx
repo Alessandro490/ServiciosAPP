8f3b08746e5acab3514d6e9807f6dbfdb35eff72
Monica <62665744+MonicaV23@users.noreply.github.com>
2024-07-08 08:49:04 -0600

    Adding valoration function

---
+++ Servicios-app/src/pages/valoracion/sistemavaloracion.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './sistemavaloracion.css'; // Asegúrate de incluir el estilo CSS
+
+const sistemavaloracion = ({ totalStars = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div className="rating">
+      {[...Array(totalStars)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "on" : "off"}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default sistemavaloracion;
